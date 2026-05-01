ef7c23e75fddc7318acb928bc68fdcc7cad8ecdf
officevmas-sketch <office.vmas@gmail.com>
2026-05-02 04:10:09 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -11,6 +11,4 @@ window.firebaseConfig = {
   measurementId: "G-MTTGDZ6B8F"
 };
 
-// Optional: add your Firebase Web Push certificate key here for lock-screen push notifications.
-// Firebase Console > Project Settings > Cloud Messaging > Web Push certificates > Generate key pair
 window.firebaseVapidKey = "BGSVb7Vpsf-I0qebQ_Ip7fynusgmtDn3ydPOSV_XHrvFxgBMpqQdUG_BLr6mlLBJC5cgrhruLHf8wqjyb2ei0c8";

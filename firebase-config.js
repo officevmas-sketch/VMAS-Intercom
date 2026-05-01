59e04f77d80a112d2dcf314c0afdffe6d8642693
officevmas-sketch <office.vmas@gmail.com>
2026-05-02 03:55:06 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,3 @@
-// Firebase configuration for VMAS Intercom
-// Realtime Database URL has been added for live call notifications.
 window.firebaseConfig = {
   apiKey: "AIzaSyBbh9bAUg21T3i94pVD50Ak8diV0qFvRCE",
   authDomain: "vmas-intercom.firebaseapp.com",
@@ -7,10 +5,7 @@ window.firebaseConfig = {
   projectId: "vmas-intercom",
   storageBucket: "vmas-intercom.firebasestorage.app",
   messagingSenderId: "430555520466",
-  appId: "1:430555520466:web:39873293db290696bfbc0b",
-  measurementId: "G-MTTGDZ6B8F"
+  appId: "1:430555520466:web:39873293db290696bfbc0b"
 };
 
-// Optional: add your Firebase Web Push certificate key here for lock-screen push notifications.
-// Firebase Console > Project Settings > Cloud Messaging > Web Push certificates > Generate key pair
-window.firebaseVapidKey = "BGSVb7Vpsf-I0qebQ_Ip7fynusgmtDn3ydPOSV_XHrvFxgBMpqQdUG_BLr6mlLBJC5cgrhruLHf8wqjyb2ei0c8";
+window.VAPID_KEY = "BGSVb7Vpsf-I0qebQ_Ip7fynusgmtDn3ydPOSV_XHrvFxgBMpqQdUG_BLr6mlLBJC5cgrhruLHf8wqjyb2ei0c8";

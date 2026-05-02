acb7902ea65712631dc24c7ea4198d8fab9da13d
officevmas-sketch <office.vmas@gmail.com>
2026-05-02 13:54:58 +0530

    Add files via upload

--- functions/index.js
+++ functions/index.js
@@ -1,42 +1,32 @@
-const admin = require("firebase-admin");
-const { onValueCreated } = require("firebase-functions/v2/database");
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
 
-admin.initializeApp();
+const {setGlobalOptions} = require("firebase-functions");
+const {onRequest} = require("firebase-functions/https");
+const logger = require("firebase-functions/logger");
 
-exports.sendIntercomPush = onValueCreated(
-  {
-    ref: "/pushQueue/{id}",
-    instance: "vmas-intercom-default-rtdb",
-    region: "asia-southeast1"
-  },
-  async (event) => {
-    const data = event.data.val();
-    if (!data || !data.token) return null;
+// For cost control, you can set the maximum number of containers that can be
+// running at the same time. This helps mitigate the impact of unexpected
+// traffic spikes by instead downgrading performance. This limit is a
+// per-function limit. You can override the limit for each function using the
+// `maxInstances` option in the function's options, e.g.
+// `onRequest({ maxInstances: 5 }, (req, res) => { ... })`.
+// NOTE: setGlobalOptions does not apply to functions using the v1 API. V1
+// functions should each use functions.runWith({ maxInstances: 10 }) instead.
+// In the v1 API, each function can only serve one request per container, so
+// this will be the maximum concurrent request count.
+setGlobalOptions({ maxInstances: 10 });
 
-    try {
-      await admin.messaging().send({
-        token: data.token,
-        notification: {
-          title: data.title || "Office Intercom Call",
-          body: data.body || "You are being called"
-        },
-        webpush: {
-          notification: {
-            icon: "/assets/icon-192.png",
-            badge: "/assets/icon-192.png",
-            vibrate: [500, 200, 500],
-            requireInteraction: true
-          },
-          fcmOptions: {
-            link: "https://officevmas-sketch.github.io/"
-          }
-        }
-      });
-    } catch (error) {
-      console.error("Push send failed:", error);
-    }
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
 
-    await event.data.ref.remove();
-    return null;
-  }
-);
+// exports.helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });

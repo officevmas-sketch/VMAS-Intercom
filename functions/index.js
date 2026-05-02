f50e8bb00fee143e245b3360d1ed7ed3d4a869ef
officevmas-sketch <office.vmas@gmail.com>
2026-05-02 13:04:45 +0530

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,13 @@
-const admin = require('firebase-admin');
-const { onValueCreated } = require('firebase-functions/v2/database');
+const admin = require("firebase-admin");
+const { onValueCreated } = require("firebase-functions/v2/database");
 
 admin.initializeApp();
 
 exports.sendIntercomPush = onValueCreated(
   {
-    ref: '/pushQueue/{id}',
-    instance: 'vmas-intercom-default-rtdb',
-    region: 'asia-southeast1'
+    ref: "/pushQueue/{id}",
+    instance: "vmas-intercom-default-rtdb",
+    region: "asia-southeast1"
   },
   async (event) => {
     const data = event.data.val();
@@ -17,21 +17,23 @@ exports.sendIntercomPush = onValueCreated(
       await admin.messaging().send({
         token: data.token,
         notification: {
-          title: data.title || 'Office Intercom Call',
-          body: data.body || 'You are being called'
+          title: data.title || "Office Intercom Call",
+          body: data.body || "You are being called"
         },
         webpush: {
           notification: {
-            icon: '/assets/icon-192.png',
-            badge: '/assets/icon-192.png',
+            icon: "/assets/icon-192.png",
+            badge: "/assets/icon-192.png",
             vibrate: [500, 200, 500],
             requireInteraction: true
           },
-          fcmOptions: { link: 'https://officevmas-sketch.github.io/' }
+          fcmOptions: {
+            link: "https://officevmas-sketch.github.io/"
+          }
         }
       });
     } catch (error) {
-      console.error('Push send failed:', error);
+      console.error("Push send failed:", error);
     }
 
     await event.data.ref.remove();
